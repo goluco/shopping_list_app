dac12382120c647cb5d79fec6aeaee8683fa6e7b
Luis Felipe <lfmarquesf@gmail.com>
2022-08-26 15:30:26 -0300

    usuário pode retirar produtos de uma receita

--- app/controllers/user_backoffice/recipe_products_controller.rb
+++ app/controllers/user_backoffice/recipe_products_controller.rb
@@ -1,4 +1,6 @@
 class UserBackoffice::RecipeProductsController < UserBackofficeController
+  before_action :set_recipe, only: %i[new create destroy]
+
   def new
     @recipe = Recipe.find(params[:recipe_id])
     @recipe_product = RecipeProduct.new
@@ -10,5 +12,18 @@ class UserBackoffice::RecipeProductsController < UserBackofficeController
     recipe_product_params = params.require(:recipe_product).permit(:product_id, :quantity)
     @recipe.recipe_products.create(recipe_product_params)
     redirect_to user_backoffice_recipe_path(@recipe.id), notice: 'Produto adicionado com sucesso'
+  end
+
+  def destroy
+    @recipe_product = RecipeProduct.find(params[:id])
+    @recipe.recipe_products.destroy(@recipe_product)
+    @recipe_product.destroy
+    redirect_to user_backoffice_recipe_path(@recipe.id), notice: 'Produto removido com sucesso'
+  end
+
+  private
+
+  def set_recipe
+    @recipe = Recipe.find(params[:recipe_id])
   end
 end
